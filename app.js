77040eb59573dbaaf0b1b8fafe7996dbb9129e45
aalawneh220 <ahmadalawneh329@gmail.com>
2022-08-15 09:12:43 -0700

    add DOM

--- app.js
+++ app.js
@@ -39,13 +39,86 @@ function employee(id,name,dep,level,img){
         new employee(1000,"ghazy samer","administration","senior"),
         new  employee(1001,"lana ali","finance","senior"),
         new  employee(1002,"tamara ayoub","markiting","senior"),
-        new employee(1003,"safi waleed","administration","junior"),
+        new employee(1003,"safi waleed","administration","mid senior"),
         new  employee(1004,"omar zaid","develobment","senior"),
-        new employee(1005,"rana saleh","develobment","senior"),
-        new employee(1006,"hadi ahmad","finance","senior"),
+        new employee(1005,"rana saleh","develobment","junior"),
+        new employee(1006,"hadi ahmad","finance","mid senior"),
         
     ];
     
     for( let i=0;i<employees.length;i++){
         console.log(employees[i].say());
     }
+
+//     let main = document.querySelector("main")
+
+// const eTable = document.createElement('table');
+// main.appendChild(eTable)
+
+// const tHeader = document.createElement('thead');
+// eTable.appendChild(tHeader);
+
+// const firstData = document.createElement("th");
+// tHeader.appendChild(firstData);
+// firstData.textContent="ID"
+
+// const secondData = document.createElement("th");
+// tHeader.appendChild(secondData);
+// secondData.textContent="FullName"
+
+// const thirdData = document.createElement("th");
+// tHeader.appendChild(thirdData);
+// thirdData.textContent="Department"
+
+// const fourthData = document.createElement("th");
+// tHeader.appendChild(fourthData);
+// fourthData.textContent="Level"
+
+// const fifthData = document.createElement("th");
+// tHeader.appendChild(fifthData);
+// fifthData.textContent="Salary"
+
+let main = document.querySelector("main")
+
+const eTable = document.createElement('table');
+main.appendChild(eTable)
+
+const tHeader = document.createElement('thead');
+eTable.appendChild(tHeader);
+
+const firstData = document.createElement("th");
+tHeader.appendChild(firstData);
+firstData.textContent="ID"
+
+const secondData = document.createElement("th");
+tHeader.appendChild(secondData);
+secondData.textContent="FullName"
+
+const thirdData = document.createElement("th");
+tHeader.appendChild(thirdData);
+thirdData.textContent="Department"
+
+const fourthData = document.createElement("th");
+tHeader.appendChild(fourthData);
+fourthData.textContent="Level"
+
+const fifthData = document.createElement("th");
+tHeader.appendChild(fifthData);
+fifthData.textContent="Salary"
+
+//   Table header ends here
+
+const arr = ['id','fullname','dep','level','salary']
+
+for (let i = 0; i < employees.length ; i++) {
+
+    const employeeRow = document.createElement('tr');
+    eTable.appendChild(employeeRow);
+
+    for (let c = 0; c < arr.length; c++) {
+        const cell = document.createElement('td')
+        employeeRow.appendChild(cell)
+        cell.textContent= employees[i][arr[c]]
+    }
+
+}
